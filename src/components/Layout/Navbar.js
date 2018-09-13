fcbd8da0a61953fe180971e677c76f2872ddf42b
unknown <kakoty.kaustabh@gmail.com>
2018-09-13 21:52:40 +0530

    'initial'

---
+++ src/components/Layout/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const navbar =  () => {
+  return (
+    <nav className="navbar navbar-dark bg-dark mb-5">
+      <span className="navbar-brand mb-0 h1 mx-auto">LyricsMate</span>
+    </nav>
+  )
+}
+
+export default navbar;
